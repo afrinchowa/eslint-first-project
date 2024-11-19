4a27505ec8bcc1d11095475ab5c95c2ee64d8485
chowa <arfinchowa524@gmail.com>
2024-11-19 10:19:01 +0600

    student route setup

---
+++ src/app/modules/student/studen.service.ts
@@ -0,0 +1,19 @@
+// import {StudentModel} from "../student.model";
+
+import { Student } from "./student.interface";
+import { StudentModel } from "./student.model";
+
+const createStudentIntoDB = async (student: Student) => {
+  const result = await StudentModel.create(student);
+  return result;
+};
+
+const getAllStudentsFromDB = async () => {
+  const result = await StudentModel.find();
+  return result;
+};
+
+export const StudentServices = {
+  createStudentIntoDB,
+  getAllStudentsFromDB,
+};
