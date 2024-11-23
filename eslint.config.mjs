c31a6f88132790cd1774191a8c0d1f27f61adc29
chowa <arfinchowa524@gmail.com>
2024-11-24 05:15:42 +0600

    student service fixing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,32 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     ignores: ["node_modules", "dist"],
     rules: {
       "no-unused-vars": "error",
-      "no-unused-expressions": "error",
-      "prefer-const":"error",
-      "no-console":"warn",
-      "no-undef":"error"
+      "no-unused-expression": "error",
+      "prefer-const": "error",
+      "no-console": "warn",
     },
-    "globals":{
-      "process":"readonly/"
-    }
+    globals: "readonly",
   },
-];
-
-// eslint.config.mjs
-const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
-
-module.exports = [
-  // Any other config imports go at the top
-  eslintPluginPrettierRecommended,
 ];
